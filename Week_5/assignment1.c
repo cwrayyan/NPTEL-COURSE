41be0fd72bb61c15278f32cccf5251b31a19c9b0
cwrayyan <rayyan.3133@gmail.com>
2026-03-28 19:31:12 +0000

    nptel

---
+++ Week_5/assignment1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n, count = 0, temp;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    temp = n;
+    while (temp != 0){
+        count++;
+        temp /= 10;
+    }
+    printf("the number of digits is %d", count);
+}
---
+++ Week_5/assignment3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int N;
+    float sum = 0;
+    int i = 1;
+    scanf("%d", &N);
+    while (i <= N ){
+        sum = sum + 1.0/i;
+        i++;
+    }
+    printf("%.2f", sum);
+}
