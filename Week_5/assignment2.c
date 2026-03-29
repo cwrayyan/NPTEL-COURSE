90816f75460dfcdded7ca2517202b8f782dd0caf
cwrayyan <rayyan.3133@gmail.com>
2026-03-29 15:54:50 +0000

    nptel

--- Week_5/assignment2.c
+++ Week_5/assignment2.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-int main(){
-    
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n<=0){
+        printf("not power of 2");
+        return 0;
+    }
+
+    while(n%2 == 0){
+        n = n / 2;
+    }
+
+    if(n == 1){
+        printf("Power of 2");
+
+    }else{
+        printf("Not power of 2");
+    }
 }
